c0633a6442ba705f5ae6aa6d7ce6f5660dade581
petrkopecky <petr.kopecky@gmail.com>
2023-07-18 17:29:30 +0200

    productService

--- src/main/java/pk/springboot/Application.java
+++ src/main/java/pk/springboot/Application.java
@@ -3,14 +3,18 @@ package pk.springboot;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "pk")
+@EnableJpaRepositories("pk.repository")
+@EntityScan("pk.entity")
 public class Application {
 
     public static void main(String[] args) {
